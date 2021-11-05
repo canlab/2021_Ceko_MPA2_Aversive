4386cb57e80298307ac0ffabf30f2d5dd6deeabf
marta <marta.ceko@gmail.com>
2021-11-05 09:32:53 -0600

    notes

--- scripts/create_predictive_model_object.m
+++ scripts/create_predictive_model_object.m
@@ -10,14 +10,14 @@
 % (2) load PLS outputs created in crossprediction_PLS.m 
 % ratings 
 % cross-validated models: 
-%       fold images (5 folds per model, for 5 models), 
+%       fold images (5 folds per model, for 5 models) ===== NEED TO ADD  
 %       cv stats: intercepts, beta weights (coefficients), dot prod, cosine sim
 %       meta data incl. subject and fold info
 % full models: 
-%       full sample images
+%       full sample images ===== NEED TO ADD  
 %       full stats: intercepts, beta weights
 
-% (3) load PLS model encoding maps 
+% (3) load PLS model encoding maps     ===== NEED TO ADD  
 
 
 % (4) Integrate into one object
@@ -31,7 +31,7 @@ cd (scriptsdir)
 load(fullfile(resultsdir, 'data_objects.mat'));
 
 
-% Load prediction outputs
+% Load prediction outputs ---- this loads STATS only for now 
 % -------------------------------------------------------------------------
 load(fullfile(resultsdir, 'PLS_crossvalidated_N55_gm.mat'));
 
